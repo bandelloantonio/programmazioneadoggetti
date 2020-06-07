515b64f9123363311fd5bc4022f6c7171cca6e4e
Antonio Bandello <bandelloantonio@libero.it>
2020-06-07 12:56:36 +0200

    Prima Commit

---
+++ Progetto/src/test/java/univpm/Progetto/ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package univpm.Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
