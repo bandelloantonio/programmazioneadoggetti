549752902704c52e21f1f74bda1d1ecb2bd1e569
Antonio Bandello <bandelloantonio@libero.it>
2020-06-07 18:15:10 +0200

    iniziata creazione di un jsonarray

---
+++ Progetto/src/main/java/univpm/Progetto/JsonArray.java
@@ -0,0 +1,63 @@
+package univpm.Progetto;
+
+public class JsonArray {
+
+}
+package esempioJSONparse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class TestJSONgetAndDecodeArray {
+
+	public static void main(String[] args) {
+
+		String url = ""; //e.g. https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/user/1.1/geo/reverse_geocode.json?lat=37.781157&long=-122.398720&granularity=neighborhood
+		String fileName = "";
+		if(args.length == 1) {
+			url = args[0]; //Url by args ;-)
+		}
+		else
+		{
+			return;
+		}
+		try {
+			
+			URLConnection openConnection = new URL(url).openConnection();
+			InputStream in = openConnection.getInputStream();
+			
+			 String data = "";
+			 String line = "";
+			 try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+			   }
+			 } finally {
+			   in.close();
+			 }
+			System.out.println( data );
+			//JSONArray obj = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
+			JSONObject obj = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
+			System.out.println( "OK" );
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
--- Progetto/src/main/java/univpm/Progetto/controller/Controller.java
+++ Progetto/src/main/java/univpm/Progetto/controller/Controller.java
@@ -1,5 +1,9 @@
 package univpm.Progetto.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+@GetMapping("/")
 
 }
