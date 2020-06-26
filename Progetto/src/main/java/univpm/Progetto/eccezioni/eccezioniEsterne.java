4250b13e958b48c0dca1a89a4996901c7000bb29
Antonio Bandello <bandelloantonio@libero.it>
2020-06-26 08:15:26 +0200

    cercando di capire il problema con le eccezioni

--- Progetto/src/main/java/univpm/Progetto/eccezioni/eccezioniEsterne.java
+++ Progetto/src/main/java/univpm/Progetto/eccezioni/eccezioniEsterne.java
@@ -2,13 +2,28 @@ package univpm.Progetto.eccezioni;
 
 import java.util.Calendar;
 
+/**
+ * Creazione di una classe per la gestione personalizzata delle eccezzioni esterne 
+ * @author Antonio
+ */
 public class eccezioniEsterne extends Exception {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
 	private String errorepresente;
+	/**
+	 * 
+	 */
 	private Calendar istante;
+	/**
+	 * 
+	 * @param istante
+	 * @param tipoerrore
+	 */
 	public eccezioniEsterne(Calendar istante,String tipoerrore) {
 		errorepresente="Le coordinate inserite presentano un errore in: "+tipoerrore;
 		this.istante=istante;
